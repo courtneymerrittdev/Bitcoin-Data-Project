57d3e705a047cd9b3dad72c9b25c98caec58003d
Courtney Merritt <Courtney.Merritt@carelincmed.com>
2024-12-26 14:52:44 -0500

    Pushing bitcoin working project onto to local repository to work on at home.

---
+++ ap.js
@@ -0,0 +1,15 @@
+var btc = document.getElementById("bitcoin");
+
+var liveprice = {
+    "async": true,
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+
+    "method": "GET",
+    "headers": {}
+}
+
+$.ajax(liveprice).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+
+});
